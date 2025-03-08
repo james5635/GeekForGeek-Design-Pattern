dec52291cddd50367ec8393e85e3ecd7f20775a7
james5635 <brorojame2029@gmail.com>
2025-03-08 09:43:31 +0000

    add uml

---
+++ AbstractFactory_Wikipedia.cpp
@@ -0,0 +1,123 @@
+#include <print>
+#include <string>
+#include <iostream>
+enum class Direction
+{
+    North,
+    South,
+    East,
+    West
+};
+class MapSite
+{
+  public:
+    virtual void enter() = 0;
+    virtual ~MapSite() = default;
+};
+class Room : public MapSite
+{
+  public:
+    Room() : roomNumber(0)
+    {
+    }
+    Room(int n) : roomNumber(n)
+    {
+    }
+    void setSide(Direction d, MapSite *mp)
+    {
+        std::println("Room::setside {} ms", 1);
+    }
+    virtual void enter()
+    {
+    }
+    Room(const Room &) = delete;
+    Room &operator=(const Room &) = delete;
+
+  private:
+    int roomNumber;
+};
+class Wall : public MapSite
+{
+  public:
+    Wall()
+    {
+    }
+    virtual void enter()
+    {
+    }
+};
+class Door : public MapSite
+{
+  public:
+    Door(Room *r1 = nullptr, Room *r2 = nullptr) : room1(r1), room2(r2)
+    {
+    }
+    virtual void enter() {}
+    Door(const Door &) = delete;
+    Door &operator=(const Door &) = delete;
+
+  private:
+    Room *room1;
+    Room *room2;
+};
+class Maze
+{
+  public:
+    void addRoom(Room *r)
+    {
+        std::println("Maze::addRoom {}", 1);
+    }
+    Room *roomNo(int) const
+    {
+        return nullptr;
+    }
+};
+class MazeFactory
+{
+  public:
+    MazeFactory() = default;
+    virtual ~MazeFactory() = default;
+    virtual Maze *makeMaze() const
+    {
+        return new Maze();
+    }
+    virtual Wall *makeWall() const
+    {
+        return new Wall();
+    }
+    virtual Room *makeRoom(int n) const
+    {
+        return new Room(n);
+    }
+    virtual Door *makeDoor(Room *r1, Room *r2)
+    {
+        return new Door(r1, r2);
+    }
+};
+class MazeGame
+{
+  public:
+    Maze *createMaze(MazeFactory &factory)
+    {
+        Maze *aMaze = factory.makeMaze();
+        Room *r1 = factory.makeRoom(1);
+        Room *r2 = factory.makeRoom(2);
+        Door *aDoor = factory.makeDoor(r1, r2);
+        aMaze->addRoom(r1);
+        aMaze->addRoom(r2);
+        r1->setSide(Direction::North, factory.makeWall());
+        r1->setSide(Direction::East, aDoor);
+        r1->setSide(Direction::South, factory.makeWall());
+        r2->setSide(Direction::North, factory.makeWall());
+        r2->setSide(Direction::East, factory.makeWall());
+        r2->setSide(Direction::South, factory.makeWall());
+        r2->setSide(Direction :: West, aDoor);
+        return aMaze;
+    }
+};
+int main()
+{
+    MazeGame game;
+    MazeFactory factory;
+    game.createMaze(factory);
+}
--- Makefile
+++ Makefile
@@ -1,21 +1,24 @@
-CXX := g++
-CXXFLAGS := -Wall -Wextra -std=c++23 $(shell pkgconf --cflags fmt)
+CXX := clang++
+CXXFLAGS := -Wall -Wextra -std=c++23 $(shell pkgconf --cflags fmt) 
 LDFLAGS := $(shell pkgconf --libs fmt)
 SRC := FactoryMethod.cpp AbstractFactory.cpp Singleton.cpp Prototype.cpp Builder.cpp Builder_Pizza.cpp Adapter.cpp Bridge.cpp \
 		Composite.cpp Composite_Wikipedia.cpp Composite_ProgrammingLanguage.cpp Decorator.cpp Decorator_Wikipedia.cpp \
-		Facade.cpp Facade_Wikipedia.cpp
+		Facade.cpp Facade_Wikipedia.cpp AbstractFactory_Wikipedia.cpp
 OUT_DIR := ./out
 BINARIES := $(patsubst %.cpp, $(OUT_DIR)/%, $(SRC))
 
-all: $(BINARIES)
+all: $(BINARIES) uml
 
 $(OUT_DIR)/%: %.cpp | $(OUT_DIR)
 	$(CXX) $(CXXFLAGS) -o $@ $< $(LDFLAGS)
 
 $(OUT_DIR):
 	mkdir -p $(OUT_DIR)
-
-clean:
+uml:
+	plantuml ./UML
+uml-clean:
+	rm ./UML/*.png
+clean: uml-clean
 	rm -rf $(OUT_DIR)
 
 .PHONY: all clean
