904a70ba4dfe54616b014e05614f18c10cc323e0
james5635 <brorojame2029@gmail.com>
2025-03-14 12:20:28 +0700

    add devcontainer

--- test/main.c
+++ test/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+int x();
 int main()
 {
-    int x();
-    
-    printf("%d", x());
+    x();
     return 0;
 }
-int x(){
+int x()
+{
+    char x[4] = "iss";
+    char y[4] = "iss\0";
+    char z[4] = {'i', 's', 's', '\0'};
+    char z1[4] = {'i', 's', 's'};
+    printf("%d, %d, %d, %d", x[3], y[3], z[3], z1[3]);
     return 10;
 }
